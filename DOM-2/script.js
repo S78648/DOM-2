094306c6f8a620c00e42ca8bec293ffca87c4350
S78648 <88738447+S78648@users.noreply.github.com>
2024-01-18 21:12:20 +0530

    Add files via upload

---
+++ DOM-2/script.js
@@ -0,0 +1,18 @@
+var elem = document.querySelectorAll('.elem');
+
+elem.forEach(function(item){
+
+    item.addEventListener('mouseenter',function(){
+        item.childNodes[3].style.opacity = 1;
+    })
+
+    item.addEventListener('mouseleave',function(){
+        item.childNodes[3].style.opacity = 0;
+    })
+
+    item.addEventListener('mousemove',function(e){
+        item.childNodes[3].style.left = `${e.x}px`;
+        /* item.childNodes[3].style.top = `${e.y}px`; */
+    })
+
+})
